c78be7a28c4f9495b4128113ae6a204573951f0a
Mustafa Şentürk <mustafa.senturk@ode.al>
2026-04-10 12:29:48 +0000

    Release SDK version 2.2.9

--- README.md
+++ README.md
@@ -15,7 +15,7 @@ Odeal Entegrasyon SDK (Otomatik Üretildi)
 <dependency>
     <groupId>com.odeal</groupId>
     <artifactId>odeal-sdk</artifactId>
-    <version>2.2.8</version>
+    <version>2.2.9</version>
 </dependency>
 ```
 
--- src/main/java/com/odeal/sdk/BaseResource.java
+++ src/main/java/com/odeal/sdk/BaseResource.java
@@ -28,7 +28,7 @@ public abstract class BaseResource {
     protected final HttpClient httpClient;
     protected final OdealConfig config;
     protected final ObjectMapper objectMapper;
-    private static final String AGENT = "OdealSdkJavaClient/2.2.8";
+    private static final String AGENT = "OdealSdkJavaClient/2.2.9";
 
     public BaseResource(OdealConfig config) {
         this.config = config;
