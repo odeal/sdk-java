1a5c0160c6b30e712df9b67e41f8bed7b04cc129
Mustafa Şentürk <mustafa.senturk@ode.al>
2026-06-23 08:11:25 +0000

    Release SDK version 2.17.2

--- src/main/java/com/odeal/sdk/BaseResource.java
+++ src/main/java/com/odeal/sdk/BaseResource.java
@@ -37,7 +37,7 @@ public abstract class BaseResource {
     protected final HttpClient httpClient;
     protected final OdealConfig config;
     protected final ObjectMapper objectMapper;
-    private static final String AGENT = "OdealSdkJavaClient/2.17.0";
+    private static final String AGENT = "OdealSdkJavaClient/2.17.2";
 
     /**
      * Dedicated thread pool for async operations.
